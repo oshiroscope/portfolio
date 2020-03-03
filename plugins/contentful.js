d19b203ab6dc4be59f83c4b13ecd6aef736956d5
oshiroscope <whitecastle4646@gmail.com>
2020-03-03 18:02:03 +0900

    add comment

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,4 +1,5 @@
 const contentful = require('contentful')
+// なぜかexport defaultだとnetlifyが失敗する
 module.exports = contentful.createClient({
   space: process.env.CTF_SPACE_ID,
   accessToken: process.env.CTF_ACCESS_TOKEN
