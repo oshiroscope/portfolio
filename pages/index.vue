4e06602cef1f22d99965b268741edc271e32441a
oshiroscope <whitecastle4646@gmail.com>
2020-03-04 18:00:58 +0900

    modify size

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,47 @@
 <template>
   <section class="background">
-    <img src="~/assets/images/teaser.png" class="teaser" v-parallax.absY="0.4">
+    <img src="~/assets/images/teaser.png" class="teaser" v-parallax.absY="0.532">
     <div class="container">
       <!-- bio -->
-      <div class="content">
-        <div class="left">
-          <div class="thumb">
-            <img :src="bio.fields.image.fields.file.url">
-          </div>
-          <div class="text">
-            <h1>{{ bio.fields.name }}</h1>
-            <div class="sentence" v-html="$md.render(bio.fields.content)" />
-            <br>
-            <div class="icon">
-              <a :href="bio.fields.twitter">
-                <font-awesome-icon :icon="['fab', 'twitter']" />
-              </a>
-              <a :href="bio.fields.instagram">
-                <font-awesome-icon :icon="['fab', 'instagram']" />
-              </a>
-              <font-awesome-icon :icon="['far', 'envelope']" />
-              <!-- <font-awesome-icon icon="envelope" /> -->
-            </div>
+      <div class="box left">
+        <div class="thumb">
+          <img :src="bio.fields.image.fields.file.url">
+        </div>
+        <div class="text">
+          <h1>{{ bio.fields.name }}</h1>
+          <div class="sentence" v-html="$md.render(bio.fields.content)" />
+          <br>
+          <div class="icon">
+            <a :href="bio.fields.twitter">
+              <font-awesome-icon :icon="['fab', 'twitter']" />
+            </a>
+            <a :href="bio.fields.instagram">
+              <font-awesome-icon :icon="['fab', 'instagram']" />
+            </a>
+            <font-awesome-icon :icon="['far', 'envelope']" />
+            <!-- <font-awesome-icon icon="envelope" /> -->
           </div>
         </div>
       </div>
       <!-- habit -->
-      <div class="content">
-        <div class="right">
-          <div class="thumb-small">
-            <video :src="bio.fields.animation.fields.file.url" loop autoplay muted playsinline />
-          </div>
-          <div class="text">
-            <h1>Habit</h1>
-            <div class="sentence" v-html="$md.render(bio.fields.habit)" />
-          </div>
+      <div class="box right">
+        <div class="thumb-small">
+          <video :src="bio.fields.animation.fields.file.url" loop autoplay muted playsinline />
+        </div>
+        <div class="text">
+          <h1>Habit</h1>
+          <div class="sentence" v-html="$md.render(bio.fields.habit)" />
         </div>
       </div>
       <!-- contents -->
-      <div class="content">
-        <div v-for="(post, index) in posts" :key="index" :class="classLeftRight(index)">
-          <div class="thumb">
-            <img :src="post.fields.image.fields.file.url">
-          </div>
-          <div class="text">
-            <h1>{{ post.fields.title }}</h1>
-            <h2>{{ post.fields.keywords }}</h2>
-            <div class="sentence" v-html="$md.render(post.fields.content)" />
-          </div>
+      <div v-for="(post, index) in posts" :key="index" :class="classLeftRight(index)">
+        <div class="thumb">
+          <img :src="post.fields.image.fields.file.url">
+        </div>
+        <div class="text">
+          <h1>{{ post.fields.title }}</h1>
+          <h2>{{ post.fields.keywords }}</h2>
+          <div class="sentence" v-html="$md.render(post.fields.content)" />
         </div>
       </div>
     </div>
@@ -60,9 +54,9 @@ export default {
   methods: {
     classLeftRight (index) {
       if (index % 2 === 0) {
-        return 'left'
+        return 'box left'
       } else {
-        return 'right'
+        return 'box right'
       }
     }
   },
@@ -83,19 +77,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+section.background{
+  /*フォントの指定*/
+  font-family: 'メイリオ', 'Meiryo', 'ヒラギノ角ゴ Pro', 'Hiragino Kaku Gothic Pro', 'Noto Sans Japanese', Osaka, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
+  margin: 0;
+  padding: 0;
+  /*サイトのデフォルトとなる文字の色を指定*/
+  color: #333;
+}
+
 /*見出しを全て太字で表示 + 文字色を濃い黒に*/
 h1, h2, h3, h4, h5, h6 {
   font-weight: bold;
   color: rgb(29, 29, 29);
-}
-
-h1 {
-  font-size: 1.9rem;
-  letter-spacing: 1px;
-}
-
-h2 {
-  font-size: 1.7rem;
 }
 
 // すべての画像・動画について親要素にサイズが追従するようにする
@@ -106,17 +100,6 @@ img, video {
   width /***/:auto;
 }
 
-section.background{
-  /*フォントの指定*/
-  font-family: 'メイリオ', 'Meiryo', 'ヒラギノ角ゴ Pro', 'Hiragino Kaku Gothic Pro', 'Noto Sans Japanese', Osaka, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
-  /*行間の指定*/
-  line-height: 1.6;
-  margin: 0;
-  padding: 0;
-  /*サイトのデフォルトとなる文字の色を指定*/
-  color: #333;
-}
-
 .teaser{
   width: 100vw; // 画面幅が画像大きい時に合わせて拡大
 }
@@ -125,64 +108,98 @@ section.background{
   background-color: #fff;
   position: relative; // z-indexのために必要
   z-index: 10;
+  display: flex;
+  flex-direction: column;
 }
 
-.content{
+.box{
   margin: 0 auto; // 中央にalignするよ
   max-width: 1400px; // 拡大しても1400pxよりは大きくならないようにするよ
   padding: 2rem;
   padding-top: 4rem;
-  .left{
-    display: flex; // CSS Gridで子要素を並べるよ
-    align-items: center;
-    flex-direction: row;
+  font-size: 20px;
+  line-height: 1.6;  /*行間の指定*/
+  display: flex; // CSS Gridで子要素を並べるよ
+  align-items: center;
+}
+.left{
+  flex-direction: row;
+}
+.right{
+  flex-direction: row-reverse;
+}
+.thumb{
+  flex: 1;
+}
+.thumb-small{
+  flex: 0.9;
+}
+.text{
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 2rem;
+  padding-bottom: 4rem;
+  h1 {
+    font-size: 2.5rem;
+    letter-spacing: 1px;
   }
-  .right{
-    display: flex; // CSS Gridで子要素を並べるよ
-    align-items: center;
-    flex-direction: row-reverse;
+  h2 {
+    font-size: 1.7rem;
   }
-  .thumb{
-    flex: 1;
+  .sentence{
+    padding-top: 2rem;
+    font-size: 20px;
+    text-align: justify;
+    text-justify: inter-word;
   }
-  .thumb-small{
-    flex: 0.9;
-  }
-  .text{
-    flex: 2;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 2rem;
-    padding-bottom: 4rem;
-    .sentence{
-      padding-top: 2rem;
-      font-size: 20px;
-      text-align: justify;
-      text-justify: inter-word;
-    }
-    .icon{
-      font-size: 2.4em;
-      color: rgb(48, 48, 48);
-    }
+  .icon{
+    font-size: 2.4em;
+    color: rgb(48, 48, 48);
   }
 }
 
-@media screen and (max-width:1000px){
-  .content{
+@media screen and (max-width:992px){
+  .box{
+    font-size: 14px;
     padding: 0.5rem;
     padding-top: 1rem;
-    .left{
-      flex-direction: column;
+    flex-direction: column;
+  }
+  .thumb{
+    width: 60%;
+  }
+  .thumb-small{
+    width: 54%;
+  }
+}
+
+@media screen and (max-width:544px){
+  .box{
+    font-size: 10px;
+    padding: 2rem;
+    flex-direction: column;
+  }
+  .thumb{
+    width: 60%;
+  }
+  .thumb-small{
+    width: 54%;
+  }
+  .text{
+    padding: 0;
+    padding-top: 1rem;
+    h1 {
+      font-size: 1.5rem;
+      letter-spacing: 1px;
     }
-    .right{
-      flex-direction: column;
+    h2 {
+      font-size: 1.1rem;
     }
-    .thumb{
-      width: 60%;
-    }
-    .thumb-small{
-      width: 54%;
+    .sentence{
+      font-size: 0.9rem;
+      padding-top: 1rem;
     }
   }
 }
